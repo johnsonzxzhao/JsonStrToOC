8a7feddb8363b49c7091e59c5482f6306f69863c
Johnson <240394381@qq.com>
2018-01-01 00:49:40 +0800

    Initial Commit

---
+++ JsonStr 转OC属性声明/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  JsonStr 转OC属性声明
+//
+//  Created by xinying on 2018/1/1.
+//  Copyright © 2018年 habav. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
